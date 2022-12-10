988fd8a4934e8b5a80a4a6e145e684041464d80e
sjk5766 <wjdrnr6662@gmail.com>
2022-12-10 16:30:16 +0900

    refactor: Product 엔티티화 및 repository 사용

--- src/main/java/kitchenpos/product/domain/Product.java
+++ src/main/java/kitchenpos/product/domain/Product.java
@@ -1,13 +1,20 @@
 package kitchenpos.product.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private BigDecimal price;
 
-    public Product() {}
+    protected Product() {}
 
     public Product(Long id, String name, BigDecimal price) {
         this.id = id;
